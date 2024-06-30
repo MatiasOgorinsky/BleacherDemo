8dc4ead65fa42cdc55bc171a36a1afeb42341754
MatiasOgorinsky <matiogorinsky@gmail.com>
2024-06-30 13:48:09 +0300

    component added

---
+++ src/app/components/GameIframe.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Header from "./Header";
+import FooterSection from "./Footer";
+
+interface GameIframeProps {
+  source: string;
+}
+
+const GameIframe: React.FC<GameIframeProps> = ({ source }) => {
+  return (
+    <div className="relative bg-[#f6f6f6]">
+      <Header />
+      <section className="max-w-8xl mx-auto p-4 px-6 md:px-36 mt-0">
+        <div className="relative bg-secondary rounded-2xl h-[700px] w-full">
+          <iframe className="absolute inset-0 p-2 w-full h-full rounded-2xl" src={source} frameBorder="0" style={{ border: "0" }} referrerPolicy="origin" allowFullScreen></iframe>
+        </div>
+      </section>
+      <FooterSection />
+    </div>
+  );
+};
+
+export default GameIframe;
