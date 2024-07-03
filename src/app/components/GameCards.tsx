e5bda999993c7ad070e8869954bddc7d991bdcfb
MatiasOgorinsky <matiogorinsky@gmail.com>
2024-07-03 15:26:53 +0300

    updates css

--- src/app/components/GameCards.tsx
+++ src/app/components/GameCards.tsx
@@ -3,12 +3,12 @@ import imageUrls from "../constants/constants";
 
 const GameCards = () => {
   return (
-    <div className="relative bg-[#f6f6f6] md:px-36 md:py-10">
-      <div className="bg-white w-full mx-auto rounded-sm p-5">
+    <div className="relative bg-gray-800 md:px-36 md:py-10">
+      <div className="bg-gray-700 w-full mx-auto rounded-sm p-5">
         <section className="p-4">
           <div className="relative grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 z-100">
             {imageUrls.map((game) => (
-              <div key={game.id} className="bg-card text-card-foreground relative w-full cursor-pointer rounded-sm shadow-none border-0">
+              <div key={game.id} className="bg-card text-white relative w-full cursor-pointer rounded-sm shadow-none border-0">
                 <a href={game.link}>
                   <div className="h-64 w-full relative flex items-center rounded-sm justify-center bg-background">
                     <img src={game.gameImage} className="h-full w-11/12 object-cover rounded-sm object-top" alt={`Game Image ${game.id}`} />
@@ -17,8 +17,8 @@ const GameCards = () => {
                     </div>
                   </div>
                   <div className="p-0">
-                    <h2 className="relative uppercase mt-2 text-3xl font-nba z-2">{game.name}</h2>
-                    <p className="leading-5 mt-1 text-sm">{game.description}</p>
+                    <h2 className="relative uppercase mt-2 text-3xl font-nba z-2 text-gray-300">{game.name}</h2>
+                    <p className="leading-5 mt-1 text-sm text-gray-300">{game.description}</p>
                   </div>
                 </a>
               </div>
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,25 +3,67 @@ import React from "react";
 const Header = () => {
   return (
     <header>
-      <div className="bg-black text-white flex items-center p-2">
-        <a href="/">
-          <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzoBABBloG39tuVccX87NmnD81Ui-z_1tS7A&s"
-            alt="Brand Logo"
-            className="h-9 w-9 ml-20" // Ajuste de márgenes
-          />
-        </a>
+      <div className="flex items-center bg-black text-gray-300 p-2 h-50">
+        <div className="ml-8">
+          <h1 style={{ margin: 0, marginRight: "4.5rem" }}>
+            <a title="BR Logo" aria-label="Bleacher Report Home" href="/">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzoBABBloG39tuVccX87NmnD81Ui-z_1tS7A&s" alt="Brand Logo" className="h-9 w-10" />
+            </a>
+          </h1>
+        </div>
+        <div className="hidden md:flex items-center space-x-6">
+          <button className="text-sm">NBA</button>
+          <a className="text-sm" href="/">
+            WORLD FOOTBALL
+          </a>
+          <button className="text-sm">NFL</button>
+          <a className="text-sm" href="/">
+            AEW
+          </a>
+          <a className="text-sm" href="/">
+            WWE
+          </a>
+          <a className="text-sm" href="/">
+            MMA
+          </a>
+          <a className="text-sm" href="/">
+            BOXING
+          </a>
+          <button className="text-sm">SPORTS</button>
+          <a className="text-sm" href="/">
+            SCORES
+          </a>
+        </div>
       </div>
-      <nav className="bg-white border-b shadow-lg">
-        <div className="flex items-center justify-start px-2 h-16 ml-20">
-          <span className="font-bold border-r border-primary pr-4">Puzzles</span>
-          <div className="flex flex-wrap gap-6 ml-4 flex-1">
-            <span className="hidden md:flex font-light text-sm">Popular</span>
-            <span className="hidden md:flex font-light text-sm">Daily Brain Teasers</span>
-            <span className="hidden md:flex font-light text-sm">Word Searches</span>
-            <span className="hidden md:flex font-light text-sm">Crossword Challenges</span>
-            <span className="hidden md:flex font-light text-sm">Logic Puzzles</span>
-            <span className="hidden md:flex font-light text-sm">Sudoku Puzzles</span>
+      <nav className="bg-black border-b shadow-lg">
+        <div className="flex items-center justify-start px-2 h-16 ml-4  md:ml-10 text-gray-300">
+          <span className="font-bold border-r border-primary pr-4 text-sm md:text-base">Puzzles</span>
+          <div className="flex flex-wrap gap-6 ml-2 md:ml-4 flex-1">
+            <a href="/" className="typography -heading-3 -text--primary text-sm md:text-base">
+              NBA
+            </a>
+            <a href="/" className="typography -heading-4 -text--tertiary navLink text-sm md:text-base">
+              NEWS
+            </a>
+            <a href="/" className="typography -heading-4 -text--tertiary navLink text-sm md:text-base">
+              SCORES
+            </a>
+            <a className="typography -heading-4 -text--tertiary navLink -disabled text-sm md:text-base">TEAMS</a>
+            <a href="/" className="typography -heading-4 -text--tertiary navLink text-sm md:text-base">
+              RUMORS
+            </a>
+            <a href="/" className="typography -heading-4 -text--tertiary navLink text-sm md:text-base">
+              HIGHLIGHTS
+            </a>
+            <a href="/" className="typography -heading-4 -text--tertiary navLink text-sm md:text-base">
+              FANTASY
+            </a>
+            <a href="/" className="typography -heading-4 -text--tertiary navLink text-sm md:text-base">
+              DRAFT
+            </a>
+            {/* <a href="/" className="typography -heading-4 -text--tertiary navLink text-sm md:text-base">
+              BR Sports on Max
+            </a> */}
           </div>
         </div>
       </nav>
