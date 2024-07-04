0b2c01f7d8b2efd6b4f83317b6e9544cc86632fe
MatiasOgorinsky <matiogorinsky@gmail.com>
2024-07-04 09:24:40 +0300

    updates

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -12,25 +12,25 @@ const Header = () => {
           </h1>
         </div>
         <div className="hidden md:flex items-center space-x-6">
-          <button className="text-sm">NBA</button>
-          <a className="text-sm" href="/">
+          <button className="text-sm font-bold">NBA</button>
+          <a className="text-sm font-bold" href="/">
             WORLD FOOTBALL
           </a>
-          <button className="text-sm">NFL</button>
-          <a className="text-sm" href="/">
+          <button className="text-sm font-bold">NFL</button>
+          <a className="text-sm font-bold" href="/">
             AEW
           </a>
-          <a className="text-sm" href="/">
+          <a className="text-sm font-bold" href="/">
             WWE
           </a>
-          <a className="text-sm" href="/">
+          <a className="text-sm font-bold" href="/">
             MMA
           </a>
-          <a className="text-sm" href="/">
+          <a className="text-sm font-bold" href="/">
             BOXING
           </a>
-          <button className="text-sm">SPORTS</button>
-          <a className="text-sm" href="/">
+          <button className="text-sm font-bold">SPORTS</button>
+          <a className="text-sm font-bold" href="/">
             SCORES
           </a>
         </div>
@@ -39,31 +39,28 @@ const Header = () => {
         <div className="flex items-center justify-start px-2 h-16 ml-4  md:ml-10 text-gray-300">
           <span className="font-bold border-r border-primary pr-4 text-sm md:text-base">Puzzles</span>
           <div className="flex flex-wrap gap-6 ml-2 md:ml-4 flex-1">
-            <a href="/" className="typography -heading-3 -text--primary text-sm md:text-base">
+            <a href="/" className="typography -heading-3 -text--primary text-sm md:text-base font-bold">
               NBA
             </a>
-            <a href="/" className="typography -heading-4 -text--tertiary navLink text-sm md:text-base">
+            <a href="/" className="typography -heading-4 -text--tertiary navLink text-sm md:text-base font-bold">
               NEWS
             </a>
-            <a href="/" className="typography -heading-4 -text--tertiary navLink text-sm md:text-base">
+            <a href="/" className="typography -heading-4 -text--tertiary navLink text-sm md:text-base font-bold">
               SCORES
             </a>
-            <a className="typography -heading-4 -text--tertiary navLink -disabled text-sm md:text-base">TEAMS</a>
-            <a href="/" className="typography -heading-4 -text--tertiary navLink text-sm md:text-base">
+            <a className="typography -heading-4 -text--tertiary navLink -disabled text-sm md:text-base font-bold">TEAMS</a>
+            <a href="/" className="typography -heading-4 -text--tertiary navLink text-sm md:text-base font-bold">
               RUMORS
             </a>
-            <a href="/" className="typography -heading-4 -text--tertiary navLink text-sm md:text-base">
+            <a href="/" className="typography -heading-4 -text--tertiary navLink text-sm md:text-base font-bold">
               HIGHLIGHTS
             </a>
-            <a href="/" className="typography -heading-4 -text--tertiary navLink text-sm md:text-base">
+            <a href="/" className="typography -heading-4 -text--tertiary navLink text-sm md:text-base font-bold">
               FANTASY
             </a>
-            <a href="/" className="typography -heading-4 -text--tertiary navLink text-sm md:text-base">
+            <a href="/" className="typography -heading-4 -text--tertiary navLink text-sm md:text-base font-bold">
               DRAFT
             </a>
-            {/* <a href="/" className="typography -heading-4 -text--tertiary navLink text-sm md:text-base">
-              BR Sports on Max
-            </a> */}
           </div>
         </div>
       </nav>
